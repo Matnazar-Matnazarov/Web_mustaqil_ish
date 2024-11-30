9e670263a30737e5a6d75364b2e50046a758d971
Dasturchi04 <matnazarmatnazarov3@gmail.com>
2024-12-01 00:27:33 +0500

    host *

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q8(6@o4#(iu00)+#aa&#l*v&xpodelvlh_^=2p_ad(eqq422hb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
